f0664dd37294eb76d546ba76f3de933dd368bcda
Melodyvsl <42814809+Melodyvsl@users.noreply.github.com>
2018-11-10 01:41:44 -0800

    implements pull request workflow for homework

--- Week3/homework/step4-bonus.js
+++ Week3/homework/step4-bonus.js
@@ -1,10 +1,33 @@
-'use strict';
 
-const values = ['a', 'b', 'c', 'd', 'a', 'e', 'f', 'c'];
+// step 4 => Bonus
 
-// Add your function here. Try and come up with a good name for this function
+const letters = ['a', 'b', 'c', 'd', 'a', 'e', 'f', 'c'];
+const numbers = [1, 2, 3, 4, 1, 5, 6, 3];
 
-// Replace `yourFunction` with the name of the function you just created
-const uniqueValues = yourFunction(values);
 
-console.log(uniqueValues);
+// SET soloution
+function getUnique_set(arr){
+  return [... new Set(arr)];
+}
+
+
+//forEach solution
+function getUnique_forEach(arr){
+  let uniqeArray = [];
+  arr.forEach(element => {
+    if(!uniqeArray.includes(element)){
+      uniqeArray.push(element);
+    }
+  });
+  return uniqeArray;
+}
+
+getUnique_set(letters);
+getUnique_set(numbers);
+
+
+getUnique_forEach(letters);
+getUnique_forEach(numbers);
+}
+
+
--- Week3/homework/step4.js
+++ Week3/homework/step4.js
@@ -2,7 +2,17 @@
 
 // Add your code here
 
-const addSix = createBase(6);
+ // step 4
+ 
+ function createBase(base) {
+    const innerFunction = function (number) {
+        return base + number;
+    };
+  
+    return innerFunction;
+  }
+  
+let addSix = createBase(6);
+console.log(addSix(10));
+console.log(addSix(21));
 
-addSix(10); // returns 16
-addSix(21); // returns 27
